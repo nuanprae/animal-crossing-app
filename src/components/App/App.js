8dd37ca26844a32767817328bce6169308871057
reiyon <nsprae@gmail.com>
2021-04-13 10:15:05 +0200

    Create functions in utils

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,69 +2,19 @@ import Header from '../Header/Header';
 import Nav from '../Nav/Nav';
 import Fish from '../Fish/Fish';
 import SeaCreatures from '../SeaCreatures/SeaCreatures';
+import Bugs from '../Bugs/Bugs';
+import * as utils from '../../utils';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './app.css';
-import Bugs from '../Bugs/Bugs';
 
-// const getWeatherData = () => {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition((position) => {
-//       const latitude = position.coords.latitude;
-//       const longitude = position.coords.longitude;
-//       console.log(latitude, longitude);
-//       const apiUrl = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
-//       fetch(apiUrl)
-//         .then((response) => response.json())
-//         .then((data) => {
-//           console.log(data);
-//         });
-//     });
-//   }
-// };
+const city = utils.getCity();
+const weatherIcon = utils.getWeatherIcon();
+const weatherDescription = utils.getWeatherDescription();
 
-// getWeatherData();
-const rawWeatherData = {
-  coord: {
-    lon: 13.4054,
-    lat: 55.5975,
-  },
-  weather: [
-    {
-      id: 803,
-      main: 'Clouds',
-      description: 'broken clouds',
-      icon: 'https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F04d.png?1499366020964',
-    },
-  ],
-  base: 'stations',
-  main: {
-    temp: 6.07,
-    feels_like: 2,
-    temp_min: 5.56,
-    temp_max: 6.67,
-    pressure: 1014,
-    humidity: 45,
-  },
-  visibility: 10000,
-  wind: {},
-  clouds: {},
-  dt: 1617885232,
-  sys: {},
-  timezone: 7200,
-  id: 2712373,
-  name: 'Genarp',
-  cod: 200,
-};
-
-const city = rawWeatherData.name;
-const weatherIcon = rawWeatherData.weather[0].icon;
-const weatherDescription = rawWeatherData.weather[0].main;
-
-const date = new Date();
-const monthName = date.toLocaleString('default', { month: 'long' });
-const dayNumber = date.toLocaleDateString('default', { day: '2-digit' });
-const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+const dayNumber = utils.getDayNumber();
+const monthName = utils.getMonthName();
+const time = utils.getTime();
 
 const App = () => {
   return (
