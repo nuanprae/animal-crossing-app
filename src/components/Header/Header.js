ab9c577dd0918440e63405db69d6a7549850338f
reiyon <nsprae@gmail.com>
2021-05-15 16:14:47 +0200

    Fix mobile view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,11 @@ const Header = (props) => {
   return (
     <header className={`header ${props.className}`}>
       <h2 className="header-text">Animal Crossing Daily Report</h2>
-      <section className="sub-header">
+      <section className="sub-header location">
         <h4>{props.location}</h4>
-        <img className="weather-icon" src={props.image} alt={props.alt} />
+        <img className="weather-icon" src={props.image} alt={props.alt} />{' '}
+      </section>
+      <section className="sub-header time">
         <h4>{props.date}</h4>
         <h4>{props.time}</h4>
         <MusicPlayer />
